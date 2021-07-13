dd90ea2b300abef44e8b0c30857f51b7996d5929
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-12 20:15:40 -0700

    Fixed a bug where input boxes that were not for the image URL were affecting the src attribute of the img element

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 /* global data */
 /* exported data */
 
+var $img = document.querySelector('div.column-half>img');
+
 function imgURL(event) {
-  var $img = document.querySelector('img');
+  if (event.target.getAttribute('name') !== 'imgURL') {
+    return;
+  }
   var newURL = event.target.value;
   $img.setAttribute('src', newURL);
-  if (event.target.value === '') {
-    $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  if (event.target.value !== '') {
+    return;
   }
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
 }
 
 document.addEventListener('input', imgURL);
