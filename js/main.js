3cf69d4d335015951767cd1e7b08d87544bb00fc
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-12 17:46:10 -0700

    Listened for 'input' events on the photoUrl input to update the src attribute of the photo preview when the input value changes and that it reset if it was empty

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 /* global data */
 /* exported data */
+
+function imgURL(event) {
+  var $img = document.querySelector('img');
+  var newURL = event.target.value;
+  $img.setAttribute('src', newURL);
+  if (event.target.value === '') {
+    $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
+}
+
+document.addEventListener('input', imgURL);
