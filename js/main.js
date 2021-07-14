4dd8cf422a0f3fa15c6380c9bbadac18b929da64
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-13 17:18:51 -0700

    Defined a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ function entryFormData(event) {
     return;
   }
   var formInputs = {
-    name: $form.elements.title.value,
+    title: $form.elements.title.value,
     imgURL: $form.elements.imgURL.value,
     notes: $form.elements.notes.value
   };
@@ -35,3 +35,32 @@ function entryFormData(event) {
 }
 
 $form.addEventListener('submit', entryFormData);
+
+function journalEntryView(event) {
+  var ul = document.querySelector('ul.padding-left-0');
+  var li = document.createElement('li');
+  li.setAttribute('class', 'row margin-bottom-li');
+
+  var imgDiv = document.createElement('div');
+  imgDiv.setAttribute('class', 'column-half');
+  var imgView = document.createElement('img');
+  imgView.setAttribute('class', 'cover img-container-view');
+  imgView.setAttribute('src', data.entries[0].imgURL);
+  imgView.setAttribute('alt', 'journal-entry-image');
+
+  var textDiv = document.createElement('div');
+  textDiv.setAttribute('class', 'column-half');
+  var h2Title = document.createElement('h2');
+  h2Title.textContent = data.entries[0].title;
+  var pNotes = document.createElement('p');
+  pNotes.textContent = data.entries[0].notes;
+
+  imgDiv.appendChild(imgView);
+  textDiv.appendChild(h2Title);
+  textDiv.appendChild(pNotes);
+  li.appendChild(imgDiv);
+  li.appendChild(textDiv);
+  ul.appendChild(li);
+}
+
+journalEntryView();
