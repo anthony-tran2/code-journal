c2960c3efaa406b836ef52365d7979428fd105e4
Anthony Tran <anthony.tran6264@yahoo.com>
2021-07-13 17:30:49 -0700

    Used a loop to create a DOM tree for each journal entry in the data model and appended it to the page when the 'DOMContentLoaded' event is fired.

--- js/main.js
+++ js/main.js
@@ -37,30 +37,32 @@ function entryFormData(event) {
 $form.addEventListener('submit', entryFormData);
 
 function journalEntryView(event) {
-  var ul = document.querySelector('ul.padding-left-0');
-  var li = document.createElement('li');
-  li.setAttribute('class', 'row margin-bottom-li');
+  for (var i = 0; i < data.entries.length; i++) {
+    var ul = document.querySelector('ul.padding-left-0');
+    var li = document.createElement('li');
+    li.setAttribute('class', 'row margin-bottom-li');
 
-  var imgDiv = document.createElement('div');
-  imgDiv.setAttribute('class', 'column-half');
-  var imgView = document.createElement('img');
-  imgView.setAttribute('class', 'cover img-container-view');
-  imgView.setAttribute('src', data.entries[0].imgURL);
-  imgView.setAttribute('alt', 'journal-entry-image');
+    var imgDiv = document.createElement('div');
+    imgDiv.setAttribute('class', 'column-half');
+    var imgView = document.createElement('img');
+    imgView.setAttribute('class', 'cover img-container-view');
+    imgView.setAttribute('src', data.entries[i].imgURL);
+    imgView.setAttribute('alt', 'journal-entry-image');
 
-  var textDiv = document.createElement('div');
-  textDiv.setAttribute('class', 'column-half');
-  var h2Title = document.createElement('h2');
-  h2Title.textContent = data.entries[0].title;
-  var pNotes = document.createElement('p');
-  pNotes.textContent = data.entries[0].notes;
+    var textDiv = document.createElement('div');
+    textDiv.setAttribute('class', 'column-half');
+    var h2Title = document.createElement('h2');
+    h2Title.textContent = data.entries[i].title;
+    var pNotes = document.createElement('p');
+    pNotes.textContent = data.entries[i].notes;
 
-  imgDiv.appendChild(imgView);
-  textDiv.appendChild(h2Title);
-  textDiv.appendChild(pNotes);
-  li.appendChild(imgDiv);
-  li.appendChild(textDiv);
-  ul.appendChild(li);
+    imgDiv.appendChild(imgView);
+    textDiv.appendChild(h2Title);
+    textDiv.appendChild(pNotes);
+    li.appendChild(imgDiv);
+    li.appendChild(textDiv);
+    ul.appendChild(li);
+  }
 }
 
-journalEntryView();
+window.addEventListener('DOMContentLoaded', journalEntryView);
